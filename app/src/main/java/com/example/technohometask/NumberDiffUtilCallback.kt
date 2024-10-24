7e4c257529d05ac8234b804a806e34061983b9d3
Pavel Nepomnyashchikh <113448657+nepavellab@users.noreply.github.com>
2024-10-24 11:00:41 +0300

    Удаление ненужного логирования

--- app/src/main/java/com/example/technohometask/NumberDiffUtilCallback.kt
+++ app/src/main/java/com/example/technohometask/NumberDiffUtilCallback.kt
@@ -1,6 +1,5 @@
 package com.example.technohometask
 
-import android.util.Log
 import androidx.recyclerview.widget.DiffUtil
 
 class NumberDiffUtilCallback(
@@ -12,12 +11,10 @@ class NumberDiffUtilCallback(
     override fun getNewListSize() = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        Log.d("areItemsTheSame()", "$oldItemPosition, $newItemPosition")
         return oldList[oldItemPosition] == newList[newItemPosition]
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        Log.d("areContentsTheSame()", "$oldItemPosition, $newItemPosition")
         return oldList[oldItemPosition] == newList[newItemPosition]
     }
 }
